026b6e343dd983e8b900c3235618185be6d107fe
HelloWorld <184126501+helloworld2540@users.noreply.github.com>
2026-01-25 13:54:39 +0800

    enhanced build.sh security

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ ME=${0##*/}
 ROOT=${0%/$ME}
 
 # just put the script to .zip
-rm -f latest_build.zip
+rm -f "$ROOT/latest_build.zip"
 7z a "$ROOT/latest_build.zip" "$ROOT"/*.sh "$ROOT"/changelog.md "$ROOT"/module.prop -x!"$ROOT/$ME"
