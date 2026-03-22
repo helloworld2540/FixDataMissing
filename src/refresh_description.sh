cadf5e14051b480fc13a31300659b9ee1b073f1d
HelloWorld <184126501+helloworld2540@users.noreply.github.com>
2026-03-22 21:45:40 +0800

    build 2.0.1 (fixed)

--- src/refresh_description.sh
+++ src/refresh_description.sh
@@ -7,24 +7,23 @@ MODDIR=${0%/*}
 
 # original description
 ORIDES="Fix Android/data, Android/obb, Android/media is missing."
-NL=$'\n'
 
 if [ -e "$REBOOT_FLAG" ]; then
-    NEW_DES="[🤔 A reboot is pending.] ${NL}"
+    NEW_DES="[🤔 A reboot is pending.] \\n"
 elif [ -e "$DAEMON_PID" ]; then
     local PID_DAEMON="$(cat "$DAEMON_PID")"
-    NEW_DES="[✅ Deamon is running on PID $PID_DAEMON]${NL}"
+    NEW_DES="[✅ Deamon is running on PID $PID_DAEMON]\\n"
     if [ "$CALL_FROM_DAEMON" = "1" ]; then
-        NEW_DES="$NEW_DES- Last fix time: $(date +"%H:%M") (✅ Auto fix)${NL}"
+        NEW_DES="$NEW_DES- Last fix time: $(date +"%H:%M") (✅ Auto fix)\\n"
     else
-        NEW_DES="$NEW_DES- Last fix time: $(date +"%H:%M") (⭕ Manual fix)${NL}"
+        NEW_DES="$NEW_DES- Last fix time: $(date +"%H:%M") (⭕ Manual fix)\\n"
     fi
-    NEW_DES="$NEW_DES- Next auto fix time: $(date -d @"$(cat "$NEXT_TIME")" +"%H:%M")${NL}"
-    NEW_DES="$NEW_DES- Click 'action' to run fix.${NL}"
+    NEW_DES="$NEW_DES- Next auto fix time: $(date -d @"$(cat "$NEXT_TIME")" +"%H:%M")\\n"
+    NEW_DES="$NEW_DES- Click 'action' to run fix.\n"
     
 else
-    NEW_DES="[❌ Deamon is not running.] ${NL}"
-    NEW_DES="$NEW_DES- Click 'action' to run fix and start deamon.${NL}"
+    NEW_DES="[❌ Deamon is not running.] \n"
+    NEW_DES="$NEW_DES- Click 'action' to run fix and start deamon.\\n"
 fi
 NEW_DES="$NEW_DES$ORIDES"
 sed -i "s#^description=.*#description=$NEW_DES#" "$PROP"
