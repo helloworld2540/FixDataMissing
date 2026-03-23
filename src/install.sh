b8a1393fa467abd24bf872b5a0eee4b4cb22ce99
HelloWorld <184126501+helloworld2540@users.noreply.github.com>
2026-03-23 10:15:34 +0800

    kill daemon on install

--- src/install.sh
+++ src/install.sh
@@ -5,8 +5,9 @@ SKIP_UNZIP=1
 unzip -d "$MODPATH" -o "$ZIPFILE" -x "install.sh" -x "changelog.md"
 chmod 777 "$MODPATH"/*.sh
 touch "$MODPATH"/".reboot"
+MODDIR="/data/adb/modules/fix_data"
 if [ -f "$MODDIR/.daemon" ]; then
     echo -e "killing daemon..."
-    kill -9 $(cat "$MODDIR/.daemon")
+    kill -9 "$(cat "$MODDIR/.daemon")"
     rm -f "$MODDIR/.daemon"
 fi
