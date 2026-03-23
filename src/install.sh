894f099d3c3a43c44cf078038ca9d68a8ed78d8b
HelloWorld <184126501+helloworld2540@users.noreply.github.com>
2026-03-23 10:10:38 +0800

    not install install.sh and changelog.md now

--- src/install.sh
+++ src/install.sh
@@ -2,6 +2,6 @@
 # install.sh
 
 SKIP_UNZIP=1
-unzip -d "$MODPATH" -o "$ZIPFILE"
+unzip -d "$MODPATH" -o "$ZIPFILE" -x "install.sh" "changelog.md"
 chmod 777 "$MODPATH"/*.sh
 touch "$MODPATH"/".reboot"
