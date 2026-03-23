9a9ef7d12153c1bbb316eae16d52bcea82595f4c
HelloWorld <184126501+helloworld2540@users.noreply.github.com>
2026-03-23 10:13:45 +0800

    fix syntax error in install.sh

--- src/install.sh
+++ src/install.sh
@@ -2,6 +2,11 @@
 # install.sh
 
 SKIP_UNZIP=1
-unzip -d "$MODPATH" -o "$ZIPFILE" -x "install.sh" "changelog.md"
+unzip -d "$MODPATH" -o "$ZIPFILE" -x "install.sh" -x "changelog.md"
 chmod 777 "$MODPATH"/*.sh
 touch "$MODPATH"/".reboot"
+if [ -f "$MODDIR/.daemon" ]; then
+    echo -e "killing daemon..."
+    kill -9 $(cat "$MODDIR/.daemon")
+    rm -f "$MODDIR/.daemon"
+fi
